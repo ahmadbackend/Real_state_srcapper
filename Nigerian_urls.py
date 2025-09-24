c35b1f5901a66c11a4723a55d9e9d8972bbfba41
Ahmad Basher <ahmadbasherjsphp@gmail.com>
2025-09-24 10:08:39 +0300

    adding proxy and production-ready

--- Nigerian_urls.py
+++ Nigerian_urls.py
@@ -1,5 +1,7 @@
 import json
+import zipfile, os
 
+from decouple import  config
 from selenium import  common,webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
@@ -10,6 +12,39 @@ from selenium.common.exceptions import NoSuchElementException
 #fastapi for converting it to api endpoint
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
+# setting proxy config
+
+proxy_host = config("proxy_host")
+proxy_port = config("proxy_port")
+
+proxy_user = config("proxy_user")
+proxy_pass = config("proxy_pass")
+
+# Add proxy authentication via extension
+manifest_json = """
+{
+  "version": "1.0.0",
+  "manifest_version": 2,
+  "name": "ProxyAuth",
+  "permissions": ["proxy","tabs","unlimitedStorage","storage","<all_urls>","webRequest","webRequestBlocking"],
+  "background": {"scripts": ["background.js"]}
+
+}
+"""
+background_js = f"""
+chrome.webRequest.onAuthRequired.addListener(
+  function handler(details) {{
+    return {{authCredentials: {{username: "{proxy_user}", password: "{proxy_pass}"}}}};
+  }},
+  {{urls: ["<all_urls>"]}},
+  ['blocking']
+);
+"""
+pluginfile = 'proxy_auth_plugin.zip'
+with zipfile.ZipFile(pluginfile, 'w') as zp:
+    zp.writestr("manifest.json", manifest_json)
+    zp.writestr("background.js", background_js)
+
 
 app = FastAPI()
 
@@ -26,8 +61,9 @@ def initialize_driver():
     chrome_options.add_argument('--disable-webrtc')
     chrome_options.add_experimental_option('prefs', {'webrtc.ip_handling_policy': 'disable_non_proxied_udp'})
     chrome_options.add_experimental_option('prefs', {'webrtc.multiple_routes_enabled': False})
-    #if proxy:
-    #    chrome_options.add_argument(f'--proxy-server={proxy}')
+    chrome_options.add_argument(f"--proxy-server=http://{proxy_host}:{proxy_port}")
+    chrome_options.add_extension(pluginfile)
+
     driver = webdriver.Chrome( options=chrome_options)
     return driver
 
@@ -84,88 +120,94 @@ def scrape_property_details(driver):
 def harvest_apartments(start_url: str, max_pages: int=25):
     apartments = []  # List to store all apartment data
 
-    driver = initialize_driver()
-    max_tries = 3
-    try:
-        driver.get(start_url)
+    max_page_tries = 3
+    max_house_tries = 3
+
 
         # Wait for an element to be present (e.g., a specific element with a CSS selector)
+        # detect last page if needed
+    current_page = 1
+
+    while True:
+        driver = initialize_driver()
         try:
             WebDriverWait(driver, 360).until(
                 EC.presence_of_element_located((By.XPATH, "//ul[@role='navigation']/li[last()-1]/a"))
             )
             print("Element found, page is ready!")
-        except:
-            print("Element not found within 30 seconds, proceeding anyway or handling error.")
-        # detect last page if needed
-        last_page_number = int(driver.find_element(By.XPATH, "//ul[@role='navigation']/li[last()-1]/a").text)
-        print(last_page_number)
-        #last_page_number = 1  # demo
-        current_page = 1
-
-        while True:
             last_page_number = int(driver.find_element(By.XPATH, "//ul[@role='navigation']/li[last()-1]/a").text)
-
-            # --- build page URL correctly ---
+            print(last_page_number)
             if "?" in start_url:
                 page_url = f"{start_url}&page={current_page}"
             else:
                 page_url = f"{start_url}?page={current_page}"
-            driver.get(start_url)
-            # Wait for an element to be present (e.g., a specific element with a CSS selector)
+            driver.get(page_url)
             try:
                 WebDriverWait(driver, 360).until(
-                    EC.presence_of_element_located((By.XPATH, "//ul[@role='navigation']/li[last()-1]/a"))
+                    EC.presence_of_element_located((By.CLASS_NAME, "wp-block-body"))
                 )
-                print("Element found, page is ready!")
-            except:
+                print("house blocks found, page is ready!")
+                page_blocks = driver.find_elements(By.CLASS_NAME, "wp-block-body")
+                print(f"Found {len(page_blocks)} listings on page {current_page}", flush=True)
+                for block in range(len(page_blocks)):
+                    try:
+                        page_blocks = driver.find_elements(By.CLASS_NAME, "wp-block-body")
+
+                        # locate the link inside the block
+                        link_elem = WebDriverWait(page_blocks[block], 360).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, "a"))
+                        )
+                        link_href = link_elem.get_attribute("href")
+
+                        # open property in same tab
+                        driver.execute_script("arguments[0].click();", link_elem)
+                        WebDriverWait(driver, 360).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, ".property-details"))
+                        )
+
+                        apartment = scrape_property_details(driver)
+                        print("Scraped:", apartment.get("title"), flush=True)
+
+                        apartments.append(apartment)
+                        max_tries = 3
+                    except Exception as e:
+                        # try three time to collect  house data then record the failure
+                        if max_house_tries > 0:
+                            block -= 1
+                            max_house_tries -= 1
+                        else:
+                            apartments.append(
+                                {"error": f"failed on listing {block + 1} of page {current_page}: {str(e)}"})
+                    finally:
+                        driver.back()
+                        driver.back()
+                        WebDriverWait(driver, 360).until(
+                            EC.presence_of_all_elements_located((By.CLASS_NAME, "wp-block-body"))
+                        )
+                        time.sleep(3)
+
+                if current_page >= last_page_number or current_page >= max_pages:
+                    driver.quit()
+                    os.remove(pluginfile)  # cleanup
+
+                    break
+                else:
+                    current_page += 1
+                    max_page_tries = 3 # reset after success harvesting
+            # record failed pages after 3 tries
+            except Exception as e:
+                if max_page_tries > 0:
+                    current_page -= 1
+                    max_page_tries -= 1
+                else:
+                    apartments.append(
+                        {"error": f"failed to harvest data ofpage number {current_page } : {str(e)}"})
                 print("Element not found within 360 seconds, proceeding anyway or handling error.")
-            # Optionally, handle the timeout (e.g., exit or retry)
-            page_blocks = driver.find_elements(By.CLASS_NAME, "wp-block-body")
-            print(f"Found {len(page_blocks)} listings on page {current_page}", flush=True)
 
-            for block in range(len(page_blocks)):
-                try:
-                    page_blocks = driver.find_elements(By.CLASS_NAME, "wp-block-body")
 
-                    # locate the link inside the block
-                    link_elem = WebDriverWait(page_blocks[block], 360).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, "a"))
-                    )
-                    link_href = link_elem.get_attribute("href")
+        except Exception as e:
+            print(f"failed to load the url{start_url}: {e}")
 
-                    # open property in same tab
-                    driver.execute_script("arguments[0].click();", link_elem)
-                    WebDriverWait(driver,360).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, ".property-details"))
-                    )
-
-                    apartment = scrape_property_details(driver)
-                    print("Scraped:", apartment.get("title"), flush=True)
-
-                    apartments.append(apartment)
-                    max_tries = 3
-                except Exception as e:
-                    # try three time to collect  house data then record the failure
-                    if max_tries > 0:
-                        block -= 1
-                        max_tries -=1
-                    else:
-                        apartments.append({"error": f"failed on listing {block + 1} of page {current_page}: {str(e)}"}) 
-                finally:
-
-                    driver.back()
-                    WebDriverWait(driver, 360).until(
-                        EC.presence_of_all_elements_located((By.CLASS_NAME, "wp-block-body"))
-                    )
-                    time.sleep(3)
-
-            if current_page >= last_page_number or current_page >= max_pages: 
-                break
-            else:
-                current_page +=1
-    finally:
-        driver.quit()
     return apartments  # Don't forget to return the results!
 
 
